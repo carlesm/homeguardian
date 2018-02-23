5c0879f3a960147ad26a690cc60814f77e586254
Carles Mateu <carlesm@carlesm.com>
2018-02-23 16:51:00 +0100

    Added sensors package

--- guardian.py
+++ guardian.py
@@ -1,18 +1,38 @@
 #!/usr/bin/env python
+import time
+
+import sensors
+
 
 class Guardian(object):
     """HomeGuardian is a security system for python"""
     def __init__(self):
         super(Guardian, self).__init__()
 
+    def create_sensors(self):
+        self.mbs = sensors.mocksensors.MockBinarySensor("sensor1")
+        self.mns = sensors.mocksensors.MockNumericSensor("sensor2")
+        pass
+
+    def get_state(self):
+        # update sensors
+        self.mbs.update_state()
+        self.mns.update_value()
+        # get values from sensors
+        value1 = self.mbs.get_state()
+        value2 = self.mns.get_value()
+        return (value1, value2)
 
     def main(self):
         # crearsensores
-        # while True:
-        #      leersensores()
+        self.create_sensors()
+        while True:
+            state = self.get_state()
         #       if condiciones:
         #           notificar()
         #       sleep(1)
+            print(state)
+            time.sleep(1)
         pass
 
 if __name__ == '__main__':
---
+++ sensors/__init__.py
@@ -0,0 +1,5 @@
+from .binary import BinarySensor
+from .main import Sensor
+from .mocksensors import MockBinarySensor
+
+__all__ = ["Sensor", "BinarySensor", "MockBinarySensor"]
