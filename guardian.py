7a437f83df1cc480afdeea482859e2a5506aa64b
Carles Mateu <carlesm@carlesm.com>
2018-02-27 15:52:35 +0100

    T

--- guardian.py
+++ guardian.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import time
-
+import pprint as pp
 import sensors
 
 
@@ -8,20 +8,39 @@ class Guardian(object):
     """HomeGuardian is a security system for python"""
     def __init__(self):
         super(Guardian, self).__init__()
+        self.binary_sensors_names = [
+            "bsensor1", "bsensor2", "bsensor3"
+        ]
+        self.numeric_sensors_names = [
+            "nsensor1", "nsensor2", "nsensor3"
+        ]
+        self.numeric_sensors = []
+        self.binary_sensors = []
+        self.sensors = {}
+        self.sensors["binary"] = {}
+        self.sensors["numeric"] = {}
 
     def create_sensors(self):
-        self.mbs = sensors.mocksensors.MockBinarySensor("sensor1")
-        self.mns = sensors.mocksensors.MockNumericSensor("sensor2")
+        for name in self.numeric_sensors_names:
+            self.numeric_sensors.append(
+                sensors.mocksensors.MockNumericSensor(name)
+            )
+        for name in self.binary_sensors_names:
+            self.binary_sensors.append(
+                sensors.mocksensors.MockBinarySensor(name)
+            )
         pass
 
     def get_state(self):
         # update sensors
-        self.mbs.update_state()
-        self.mns.update_value()
-        # get values from sensors
-        value1 = self.mbs.get_state()
-        value2 = self.mns.get_value()
-        return (value1, value2)
+        for sensor in self.binary_sensors:
+            sensor.update_state()
+            self.sensors["binary"][sensor.name] = sensor.get_state()
+
+        for sensor in self.numeric_sensors:
+            sensor.update_value()
+            self.sensors["numeric"][sensor.name] = sensor.get_value()
+        return self.sensors
 
     def main(self):
         # crearsensores
@@ -31,7 +50,13 @@ class Guardian(object):
         #       if condiciones:
         #           notificar()
         #       sleep(1)
-            print(state)
+            pp.pprint(state, indent=4,width=30)
+            for bs in self.sensors["binary"]:
+                if self.sensors["binary"][bs]:
+                    print "*",
+                else:
+                    print ".",
+            print
             time.sleep(1)
         pass
 
